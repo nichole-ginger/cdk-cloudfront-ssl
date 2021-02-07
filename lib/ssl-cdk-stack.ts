0ddf844b5dd7594aa1c0e31a328e1f9633117095
Nichole Dwight <nicholedwight@gmail.com>
2021-02-07 12:50:15 -0700

    OMG IT WORKED - last up is route53

--- lib/ssl-cdk-stack.ts
+++ lib/ssl-cdk-stack.ts
@@ -1,10 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 import { ValidationMethod } from "@aws-cdk/aws-certificatemanager";
 import * as cm from '@aws-cdk/aws-certificatemanager';
+import * as s3 from '@aws-cdk/aws-s3'
 import * as ssm from '@aws-cdk/aws-ssm';
+import { CloudFrontWebDistribution } from '@aws-cdk/aws-cloudfront'
+import * as cloudfront from '@aws-cdk/aws-cloudfront';
+import { bucketRef } from './myconstants';
 
 
 const domainName = "nichole.is";
+const bucket = bucketRef;
 
 export class SslCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct) {
@@ -14,6 +19,7 @@ export class SslCdkStack extends cdk.Stack {
 
     const certificate = new cm.Certificate(this, "CustomDomainCertificate", {
       domainName: domainName,
+      subjectAlternativeNames: ['*.nichole.is'],
       validationMethod: ValidationMethod.DNS,
     });
 
@@ -21,5 +27,46 @@ export class SslCdkStack extends cdk.Stack {
     new cdk.CfnOutput(this, "CertificateArn", {
       value: certificateArn,
     });
+
+    //CloudFront Distribution 
+    // const distribution = new CloudFrontWebDistribution(this, 'cdk-example-cfront', {
+    //   originConfigs: [
+    //     {
+    //       s3OriginSource: {
+    //         s3BucketSource: sourceBucket
+    //       },
+    //       behaviors : [ {isDefaultBehavior: true}]
+    //     }
+    //   ],
+    //   aliasConfiguration: {
+    //     acmCertRef: certificateArn,
+    //     names: ['cdk-example.awsexamples.dev']
+    //   }
+    // });
+    const sourceBucket = new s3.Bucket(this, 'TestBucket',{
+            versioned: false,
+            removalPolicy: cdk.RemovalPolicy.DESTROY
+          });
+
+    const distribution = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "Distribution",
+      {
+        viewerCertificate: {
+          aliases: ['cdk.nichole.is'],
+          props: {
+            acmCertificateArn: certificateArn,
+            sslSupportMethod: "sni-only",
+          },
+        },
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: sourceBucket,
+            },
+            behaviors: [{ isDefaultBehavior: true }],
+          },
+        ],
+      });
   }
 }
