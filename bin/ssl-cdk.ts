4f698dc5cdb70795733e468b5af353bd40c39d92
Nichole Dwight <nicholedwight@gmail.com>
2021-02-10 16:14:47 -0700

    removing extra import

--- bin/ssl-cdk.ts
+++ bin/ssl-cdk.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { SslCdkStack } from '../lib/ssl-cdk-stack';
-import { account, region } from '../lib/myconstants';
+import { account } from '../lib/myconstants';
 
 const app = new cdk.App();
 new SslCdkStack(app, 'ssl-cdk-stack', {});
